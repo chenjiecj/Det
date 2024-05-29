22f7563325e553d32c2fb97fef124e5e91d10d40
chenjiecj <157132984+chenjiecj@users.noreply.github.com>
2024-05-29 11:44:11 +0800

    Update agentattention.py

--- nets/agentattention.py
+++ nets/agentattention.py
@@ -60,7 +60,11 @@ class AgentAttention(nn.Module):
         trunc_normal_(self.ca_bias, std=.02)
         pool_size = int(agent_num ** 0.5)
         self.pool = nn.AdaptiveAvgPool2d(output_size=(pool_size, pool_size))
-
+    @staticmethod
+    def with_pos_embed(tensor, pos=None):
+        """Add position embeddings to the tensor if provided."""
+        return tensor if pos is None else tensor + pos
+        
     def forward(self, x):
         """
         Args:
@@ -170,6 +174,25 @@ class AAIFI(AgentBlock):
         #x = super().forward(x.flatten(2).permute(0, 2, 1), pos=pos_embed.to(device=x.device, dtype=x.dtype))
         x = super().forward(x.flatten(2).permute(0, 2, 1))
         return x.permute(0, 2, 1).view([-1, c, h, w]).contiguous()
+        
+    """This code should be commented out. No Position embedding is required"""
+    @staticmethod
+    def build_2d_sincos_position_embedding(w, h, embed_dim=256, temperature=10000.0):
+        """Builds 2D sine-cosine position embedding."""
+        grid_w = torch.arange(int(w), dtype=torch.float32)
+        grid_h = torch.arange(int(h), dtype=torch.float32)
+        grid_w, grid_h = torch.meshgrid(grid_w, grid_h, indexing='ij')
+        #grid_w, grid_h = torch.meshgrid(grid_w, grid_h)
+        assert embed_dim % 4 == 0, \
+            'Embed dimension must be divisible by 4 for 2D sin-cos position embedding'
+        pos_dim = embed_dim // 4
+        omega = torch.arange(pos_dim, dtype=torch.float32) / pos_dim
+        omega = 1. / (temperature ** omega)
 
+        out_w = grid_w.flatten()[..., None] @ omega[None]
+        out_h = grid_h.flatten()[..., None] @ omega[None]
 
+        return torch.cat([torch.sin(out_w), torch.cos(out_w), torch.sin(out_h), torch.cos(out_h)], 1)[None]
+
+    """This code should be commented out. No Position embedding is required"""
 
